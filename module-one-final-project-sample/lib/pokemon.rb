196b7ae51fecefadd7b81a1489ffd219cbf7d8db
Esmery Corniel <esmeryjose@users.noreply.github.com>
2018-01-25 10:38:03 -0500

    fix pokemon class

--- module-one-final-project-sample/lib/pokemon.rb
+++ module-one-final-project-sample/lib/pokemon.rb
@@ -3,17 +3,19 @@ class Pokemon < ActiveRecord::Base
   has_many :trainers, through: :trainer_pokemons
 
   def self.find_or_create_through_hash(pokemon_hash)
-    parse_hash(pokemon_hash)
+    pokemon = parse_hash(pokemon_hash)
+    where(pokemon).first_or_create
   end
 
-  def self.parse_hash(hash)
-    pokemon ={}
-    hash.each do |k,v|
-      if v == "id"
-        
-        binding.pry
+  def self.parse_hash(pokemon_hash)
+    pokemon = {}
+    pokemon_hash.each do |k,v|
+      if k =="id"
+        pokemon["pokemon_number"] = v
+      elsif k =="weight" || k=="name"
+        pokemon[k] = v
       end
     end
-
+    pokemon
   end
 end
